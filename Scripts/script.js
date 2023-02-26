84d11f3e43e939e36fab5a7b6708b2952158f208
Phan Vũ Tuấn Anh <88922348+MarcoPhanVu@users.noreply.github.com>
2023-02-26 16:53:35 +0700

    add simple Shopee's images' url extractor

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,9 +1,18 @@
-let allSmallContainer = document.querySelectorAll(".result")
+const mainBody = document.getElementById('main-body');
 
+function shopeeImgUrlExtractor(ele) {
+    let picURL = ele.split(" ")[4].split("&quot")[1].split(";")[1];
 
+    const imgContainer = document.createElement("img");
+    imgContainer.src = picURL;
+    imgContainer.classList.add("typical-img");
+    mainBody.appendChild(imgContainer);
+}
 
+shopeeImgUrlExtractor(`<div class="fus9wz qO2bZw" style="background-image: url(&quot;https://cf.shopee.vn/file/sg-11134201-22120-trz7vkw6mzkvbf&quot;); background-size: contain; background-repeat: no-repeat;"></div>`);
 
-// This shit worked
-// allSmallContainer.forEach(result => {
-//     result.innerHTML = "received";
-// })
+shopeeImgUrlExtractor(`<div class="fus9wz qO2bZw" style="background-image: url(&quot;https://cf.shopee.vn/file/sg-11134201-22120-6de8lkw6mzkvb1&quot;); background-size: contain; background-repeat: no-repeat;"></div>`);
+
+shopeeImgUrlExtractor(`<div class="fus9wz qO2bZw" style="background-image: url(&quot;https://cf.shopee.vn/file/5d8b0f409e7500204b57b6192eae78de&quot;); background-size: contain; background-repeat: no-repeat;"></div>`);
+
+shopeeImgUrlExtractor(`<div class="fus9wz qO2bZw" style="background-image: url(&quot;https://cf.shopee.vn/file/sg-11134201-22100-9eng9hk7w9iv2d&quot;); background-size: contain; background-repeat: no-repeat;"></div>`);
